f4a6e79a8b4383ece9daae776e8975efb6ea4e7b
gavwes <gavinsimpkins@icloud.com>
2025-11-06 15:15:55 -0500

    round robin func and helper functions

--- sched.c
+++ sched.c
@@ -102,3 +102,127 @@ struct task* create(int arrival_time, int service_time, struct task* current_nod
     current_node->next = new_node;
     //return;
 }
+
+void roundRobin(struct task *allTasks, int timeSlice, int overhead, FILE *fp, double average_response, int *total_overhead) {
+	int currentTime = 0;
+	*total_overhead = 0;
+	
+	struct task *futureTasks = allTasks;
+	struct task *readyQueue = NULL;
+	struct task *completedList = NULL;
+	struct task *cpu = NULL;
+
+	int sliceRemaining = 0;
+	int overheadRemaining = 0;
+
+	printf("%-5s %-10s %-10s %-10s %s\n", "Time", "CPU", "Remaining", "Service", "Ready Queue");
+        fprintf(fp, "%-5s %-10s %-10s %-10s %s\n", "Time", "CPU", "Remaining", "Service", "Ready Queue");
+        printf("------------------------------------------------------------------\n");
+        fprintf(fp, "------------------------------------------------------------------\n");
+
+	while (futureTasks != NULL || readyQueue != NULL || cpu != NULL || overheadRemaining > 0) {
+		while (futureTasks != NULL && futureTasks->arrival_time == currentTime) {
+			struct task *arrivedTask = futureTasks;
+			futureTasks = futureTasks->next;
+			arrivedTask->next = NULL;
+			enqueue(&readyQueue, arrivedTask);
+		}
+		printf("%-5d ", currentTime);
+        	fprintf(fp, "%-5d ", currentTime);
+        	
+		if (cpu != NULL) {
+            		printf("%-10d %-10d %-10d ", cpu->task_id, cpu->remaining_time, cpu->service_time);
+            		fprintf(fp, "%-10d %-10d %-10d ", cpu->task_id, cpu->remaining_time, cpu->service_time);
+        	} 
+		else if (overheadRemaining > 0) {
+            		printf("%-10s %-10s %-10s ", "OVERHEAD", "--", "--");
+            		fprintf(fp, "%-10s %-10s %-10s ", "OVERHEAD", "--", "--");
+        	} 
+		else {
+            		printf("%-10s %-10s %-10s ", "IDLE", "--", "--");
+            		fprintf(fp, "%-10s %-10s %-10s ", "IDLE", "--", "--");
+        	}
+
+        	print_queue(fp, readyQueue);
+        	printf("\n");
+        	fprintf(fp, "\n");
+		
+		if (cpu != NULL) {
+			cpu->remaining_time--;
+			sliceRemaining--;
+			
+			if (cpu->remaining_time == 0) {
+				cpu->completion_time = currentTime + 1;
+				enqueue(&completedList, cpu);
+				cpu = NULL;
+
+				if (readyQueue != NULL) {
+					overheadRemaining = overhead;
+					*total_overhead += overhead;
+				}
+			}
+			else if (sliceRemaining == 0) {
+				enqueue(&readyQueue, cpu);
+				cpu = NULL;
+				
+				if (readyQueue != NULL) {
+					overheadRemaining = overhead;
+					*total_overhead += overhead;
+				}
+			}
+		}
+		else if (overheadRemaining > 0) {
+			overheadRemaining--;
+
+			if (overheadRemaining == 0) {
+				cpu = dequeue(&readyQueue);
+
+				sliceRemaining = (cpu->remaining_time < timeSlice) ? cpu->remaining_time : timeSlice;
+
+				if (cpu->remaining_time == cpu->service_time) {
+					cpu->response_time = (currentTime + 1) - cpu->arrival_time;
+				}
+			}
+		}
+		else if (readyQueue != NULL) {
+			overheadRemaining = overhead;
+			*total_overhead += overhead;
+		}
+
+		currentTime++;
+	}
+	printf("------------------------------------------------------------------\n");
+ 	fprintf(fp, "------------------------------------------------------------------\n");
+	printf("All tasks complete.\n");
+	fprintf(fp, "All tasks complete.\n");
+    
+	//print_stats(fp, completedList, *total_overhead, avg_resp_time);
+	free_list(completedList);
+}
+
+void enqueue(struct task **queueHead, struct task *taskToAdd) {
+    taskToAdd->next = NULL; 
+
+    if (*queueHead == NULL) {
+        *queueHead = taskToAdd;
+    } else {
+        struct task *current = *queueHead;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = taskToAdd;
+    }
+}
+
+struct task* dequeue(struct task **queueHead) {
+    if (*queueHead == NULL) {
+        return NULL;
+    }
+    
+    struct task *taskToRun = *queueHead;
+    *queueHead = (*queueHead)->next;
+    
+    taskToRun->next = NULL; 
+    return taskToRun;
+}
+
