df20ca3b95786baa24c9b467ba304eefaf437f39
gavwes <gavinsimpkins@icloud.com>
2025-11-07 09:48:48 -0500

    made 2nd queue and appending into tail now, slightly changed create node, changed print queue to print both of my used queues and not since for some reason when a new task arrives, its supposed to take #1 priority

--- sched.c
+++ sched.c
@@ -4,45 +4,45 @@
 
 struct task* read_file_return_list(FILE *input_file, int task_id);
 struct task* create_node(int arrival_time, int service_time, int current_task_id);
-
 void roundRobin(struct task *allTasks, int timeSlice, int overhead, FILE *fp, double *avg_resp_time, int *total_overhead);
-void enqueue(struct task **queueHead, struct task *taskToAdd);
+void enqueue_tail(struct task **queueHead, struct task *taskToAdd);
 struct task* dequeue(struct task **queueHead);
-void print_queue(FILE *fp, struct task *queue);
+void print_two_queues(FILE *fp, struct task *q1, struct task *q2);
 void free_list(struct task *list);
 
 struct task{
-    int
-        task_id, 
-        service_time,
-        remaining_time,
-        completion_time,
-        response_time,
-        wait_time,
-        arrival_time;
-        struct task *next;
+    int task_id;
+    int service_time;
+    int remaining_time;
+    int completion_time;
+    int response_time;
+    int wait_time;
+    int arrival_time;
+    int has_run;
+    struct task *next;
 };
 
 int main (int argc, char *argv[]){
 
     int opt;
-    int t, i, o; 
-    char* output_file; 
+    int t, i, o; //CLAs
+    char* output_file; //Output file to be created and written to
 
+    //GetOPt procedure to get CLAs from user
     while((opt = getopt(argc, argv, "t:i:o:f:")) != -1)
     {
         switch(opt)
         {
-            case 't': 
+            case 't': //Time Slice
                 t = atoi(optarg);
                 break;
-            case 'i': 
+            case 'i': //Starting Task ID
                 i = atoi(optarg);
                 break;
-            case 'o': 
+            case 'o': //Overhead
                 o = atoi(optarg);
                 break;
-            case 'f': 
+            case 'f': //Output file
                 output_file = optarg;
                 break;
             case ':':
@@ -54,23 +54,22 @@ int main (int argc, char *argv[]){
         }
     }
 
-    struct task* head = read_file_return_list(stdin, i); 
+    struct task* head = read_file_return_list(stdin, i); //Create linked list
 
     FILE *fptr = fopen(output_file, "w");
     double avg_r_time = 0;
     int t_overhead = 0;
     roundRobin(head, t, o, fptr, &avg_r_time, &t_overhead);
 
-    fclose(fptr);
     return 0;
 }
-
 
 struct task* read_file_return_list(FILE *input_file, int task_id){
     char ch;
 
     int file_arrival_time, file_service_time;
     
+    //For each line of text of input file, pass in data to new node of linked list
     int j = 0;
     struct task* head;
     while (fscanf(input_file, "%i %i", &file_arrival_time, &file_service_time) == 2) {
@@ -89,194 +88,167 @@ struct task* read_file_return_list(FILE *input_file, int task_id){
         task_id++;
         
     }
+    /*struct task* rover = head;
+    while (rover->next != NULL)
+    {
+        rover = rover->next;
+    }
+    rover->next = head; //Complete circular list*/
+
+    fclose(input_file);
 
     return head;
 }
 
-
-struct task* create_node(int arrival_time, int service_time, int current_task_id)
-{
+struct task* create_node(int arrival_time, int service_time, int current_task_id){
     struct task* new_node = (struct task*)malloc(sizeof(struct task));
-    
     new_node->service_time = service_time;
     new_node->arrival_time = arrival_time;
     new_node->remaining_time = service_time;
-    
     new_node->completion_time = 0;
     new_node->response_time = 0;
     new_node->wait_time = 0;
     new_node->task_id = current_task_id;
-    
-    new_node->next = NULL; 
-    
+    new_node->has_run = 0;
+    new_node->next = NULL;
     return new_node;
 }
 
 void roundRobin(struct task *allTasks, int timeSlice, int overhead, FILE *fp, double *avg_resp_time, int *total_overhead) {
     int currentTime = 0;
     *total_overhead = 0;
-    
+
     struct task *futureTasks = allTasks;
-    struct task *readyQueue = NULL;
+    struct task *newQ = NULL;
+    struct task *rrQ = NULL;
     struct task *completedList = NULL;
     struct task *cpu = NULL;
-
     int sliceRemaining = 0;
-    int overheadRemaining = 0;
 
-    printf("%-5s %-10s %-10s %-10s %s\n", "Time", "CPU", "serv", "remaining", "ready");
-    fprintf(fp, "%-5s %-10s %-10s %-10s %s\n", "Time", "CPU", "serv", "remaining", "queue");
-    printf("------------------------------------------------------------------\n");
-    fprintf(fp, "------------------------------------------------------------------\n");
+    long long sum_resp_time = 0;
+    int tasks_completed = 0;
 
-    while (futureTasks != NULL || readyQueue != NULL || cpu != NULL || overheadRemaining > 0) {
-        
+    printf("Round Robin scheduling results\n\n");
+    fprintf(fp, "Round Robin scheduling results\n\n");
+    printf("%-5s %-6s %-12s %-12s %s\n", "time", "cpu", "serv time", "remaining", "ready queue");
+    fprintf(fp, "%-5s %-6s %-12s %-12s %s\n", "time", "cpu", "serv time", "remaining", "ready queue");
+    printf("---------------------------------------------------------------\n");
+    fprintf(fp, "---------------------------------------------------------------\n");
+
+    while (futureTasks != NULL || newQ != NULL || rrQ != NULL || cpu != NULL) {
         while (futureTasks != NULL && futureTasks->arrival_time == currentTime) {
             struct task *arrivedTask = futureTasks;
             futureTasks = futureTasks->next;
             arrivedTask->next = NULL;
-            enqueue(&readyQueue, arrivedTask);
-        }
-        
-        printf("%-5d ", currentTime);
-        fprintf(fp, "%-5d ", currentTime);
-        
-        if (cpu != NULL) {
-            printf("%-10d %-10d ", cpu->task_id, cpu->service_time);
-            fprintf(fp, "%-10d %-10d ", cpu->task_id, cpu->service_time);
-            
-            if (cpu->remaining_time - 1 == 0) {
-                printf("%-10s ", "0 (done)");
-                fprintf(fp, "%-10s ", "0 (done)");
-            } else {
-                printf("%-10d ", cpu->remaining_time - 1);
-                fprintf(fp, "%-10d ", cpu->remaining_time - 1);
-            }
-        } else {
-            printf("%-10d %-10d %-10s ", 0, 0, "");
-            fprintf(fp, "%-10d %-10d %-10s ", 0, 0, "");
+            enqueue_tail(&newQ, arrivedTask);
         }
 
-        print_queue(fp, readyQueue);
+        if (cpu != NULL) {
+            if (cpu->remaining_time == 0) {
+                cpu->completion_time = currentTime;
+                sum_resp_time += cpu->response_time;
+                tasks_completed++;
+                enqueue_tail(&completedList, cpu);
+                cpu = NULL;
+            } else if (sliceRemaining == 0) {
+                cpu->has_run = 1;
+                enqueue_tail(&rrQ, cpu);
+                cpu = NULL;
+            }
+        }
+
+        if (cpu == NULL) {
+            if (newQ != NULL) {
+                cpu = dequeue(&newQ);
+                *total_overhead += overhead;
+                sliceRemaining = (cpu->remaining_time < timeSlice) ? cpu->remaining_time : timeSlice;
+                if (cpu->has_run == 0) {
+                    cpu->response_time = currentTime - cpu->arrival_time;
+                    cpu->has_run = 1;
+                }
+            } else if (rrQ != NULL) {
+                cpu = dequeue(&rrQ);
+                *total_overhead += overhead;
+                sliceRemaining = (cpu->remaining_time < timeSlice) ? cpu->remaining_time : timeSlice;
+            }
+        }
+
+        if (cpu != NULL) {
+            printf("%-5d %-6d %-12d ", currentTime, cpu->task_id, cpu->service_time);
+            fprintf(fp, "%-5d %-6d %-12d ", currentTime, cpu->task_id, cpu->service_time);
+            int nextRem = cpu->remaining_time - 1;
+            if (nextRem == 0) {
+                printf("%-12s ", "0 (done)");
+                fprintf(fp, "%-12s ", "0 (done)");
+            } else {
+                printf("%-12d ", nextRem);
+                fprintf(fp, "%-12d ", nextRem);
+            }
+        } else {
+            printf("%-5d %-6s %-12s %-12s ", currentTime, "", "", "");
+            fprintf(fp, "%-5d %-6s %-12s %-12s ", currentTime, "", "", "");
+        }
+
+        print_two_queues(fp, newQ, rrQ);
         printf("\n");
         fprintf(fp, "\n");
-        
+
         if (cpu != NULL) {
             cpu->remaining_time--;
             sliceRemaining--;
-            
-            if (cpu->remaining_time == 0) {
-                cpu->completion_time = currentTime + 1;
-                enqueue(&completedList, cpu);
-                cpu = NULL;
-
-                if (readyQueue != NULL) {
-                    if (overhead == 0) {
-                        cpu = dequeue(&readyQueue);
-                        sliceRemaining = (cpu->remaining_time < timeSlice) ? cpu->remaining_time : timeSlice;
-                        if (cpu->remaining_time == cpu->service_time) {
-                            cpu->response_time = (currentTime + 1) - cpu->arrival_time;
-                        }
-                    } else {
-                        overheadRemaining = overhead;
-                        *total_overhead += overhead;
-                    }
-                }
-            }
-            else if (sliceRemaining == 0) {
-                enqueue(&readyQueue, cpu);
-                cpu = NULL;
-                
-                if (readyQueue != NULL) {
-                    if (overhead == 0) {
-                        cpu = dequeue(&readyQueue);
-                        sliceRemaining = (cpu->remaining_time < timeSlice) ? cpu->remaining_time : timeSlice;
-                        if (cpu->remaining_time == cpu->service_time) {
-                            cpu->response_time = (currentTime + 1) - cpu->arrival_time;
-                        }
-                    } else {
-                        overheadRemaining = overhead;
-                        *total_overhead += overhead;
-                    }
-                }
-            }
-        }
-        else if (overheadRemaining > 0) {
-            overheadRemaining--;
-
-            if (overheadRemaining == 0) {
-                cpu = dequeue(&readyQueue);
-
-                sliceRemaining = (cpu->remaining_time < timeSlice) ? cpu->remaining_time : timeSlice;
-
-                if (cpu->remaining_time == cpu->service_time) {
-                    cpu->response_time = (currentTime + 1) - cpu->arrival_time;
-                }
-            }
-        }
-        else if (readyQueue != NULL) {
-            if (overhead == 0) {
-                cpu = dequeue(&readyQueue);
-                sliceRemaining = (cpu->remaining_time < timeSlice) ? cpu->remaining_time : timeSlice;
-                if (cpu->remaining_time == cpu->service_time) {
-                    cpu->response_time = (currentTime + 1) - cpu->arrival_time;
-                }
-            } else {
-                overheadRemaining = overhead;
-                *total_overhead += overhead;
-            }
+	    cpu->service_time--;
         }
 
         currentTime++;
     }
-    printf("------------------------------------------------------------------\n");
-    fprintf(fp, "------------------------------------------------------------------\n");
+
+    printf("---------------------------------------------------------------\n");
+    fprintf(fp, "---------------------------------------------------------------\n");
     printf("All tasks complete.\n");
     fprintf(fp, "All tasks complete.\n");
-    
+
+    if (tasks_completed > 0) *avg_resp_time = (double)sum_resp_time / (double)tasks_completed;
+    else *avg_resp_time = 0.0;
+
     free_list(completedList);
 }
 
-void enqueue(struct task **queueHead, struct task *taskToAdd) {
-    taskToAdd->next = NULL; 
-
+void enqueue_tail(struct task **queueHead, struct task *taskToAdd) {
+    taskToAdd->next = NULL;
     if (*queueHead == NULL) {
         *queueHead = taskToAdd;
     } else {
         struct task *current = *queueHead;
-        while (current->next != NULL) {
-            current = current->next;
-        }
+        while (current->next != NULL) current = current->next;
         current->next = taskToAdd;
     }
 }
 
 struct task* dequeue(struct task **queueHead) {
-    if (*queueHead == NULL) {
-        return NULL;
-    }
-    
+    if (*queueHead == NULL) return NULL;
     struct task *taskToRun = *queueHead;
     *queueHead = (*queueHead)->next;
-    
-    taskToRun->next = NULL; 
+    taskToRun->next = NULL;
     return taskToRun;
 }
 
-void print_queue(FILE *fp, struct task *queue) {
-    if (queue == NULL) {
-        return;
+void print_two_queues(FILE *fp, struct task *q1, struct task *q2) {
+    int first = 1;
+    struct task *cur = q1;
+    while (cur != NULL) {
+        if (!first) { printf(", "); fprintf(fp, ", "); }
+        printf("%d-%d", cur->task_id, cur->remaining_time);
+        fprintf(fp, "%d-%d", cur->task_id, cur->remaining_time);
+        first = 0;
+        cur = cur->next;
     }
-
-    struct task *current = queue;
-    while (current != NULL) {
-        printf("%d-%d", current->task_id, current->remaining_time);
-        fprintf(fp, "%d-%d", current->task_id, current->remaining_time);
-        if (current->next != NULL) {
-            printf(", ");
-            fprintf(fp, ", ");
-        }
-        current = current->next;
+    cur = q2;
+    while (cur != NULL) {
+        if (!first) { printf(", "); fprintf(fp, ", "); }
+        printf("%d-%d", cur->task_id, cur->remaining_time);
+        fprintf(fp, "%d-%d", cur->task_id, cur->remaining_time);
+        first = 0;
+        cur = cur->next;
     }
 }
 
@@ -289,3 +261,4 @@ void free_list(struct task *list) {
         current = next;
     }
 }
+
